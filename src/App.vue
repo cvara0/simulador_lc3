dc3fdd076c1be4c24c1e6135399e9c8610416038
cvara <cvara.parra@gmail.com>
2024-11-30 02:57:11 -0300

    halt y contador de instrucciones, experimentos fallidos con la memoria simulada

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
 import { ref, reactive, onMounted, computed,onUnmounted, watch } from "vue"
 import Registros from "./components/Registros.vue"
 
+
+
 const registros = ref([])
-const memoriaRam = ref(new Int16Array(16384))
+const memoriaRam = ref(new Int16Array(32768))
 
 const aBuscar = ref('')
 const errores = ref([])
@@ -20,7 +22,22 @@ const insTextoHexa = reactive({
 5664
 96FF
 33FF
-29FE`,
+29FE
+6A84
+7643
+1CA0
+1DBF
+07FE
+1CA0
+1DBF
+03FE
+1CE0
+1DA1
+0DFE
+1CE0
+1DA1
+09FE
+C040`,
 })
 
 const inicio = ref("3000")
@@ -28,13 +45,6 @@ const desplazamiento = ref(12285)
 const pc = ref(12288)
 
 const scrollDelta = ref(0)
-
-const totalRows = 65000; // Total de filas
-const rowsPerPage = 20; // Filas visibles al mismo tiempo
-const buffer = 5; // Buffer adicional para un scroll fluido
-const rowHeight = 30; // Altura de cada fila en px
-const startIndex = ref(0);
-
 
 onMounted(() => {
   limpiarMemoria()
@@ -46,6 +56,13 @@ onUnmounted(() => {
       window.removeEventListener('wheel', handleWheel)
     })
 
+const limpiarMemoria = () => {
+  errorIns.value = false
+  memoriaRam.value = new Int16Array(32768)
+}
+const limpiarRegistros = () => {
+  registros.value = new Array(8).fill("0000")
+}
 
 const handleWheel = (event) => {
       scrollDelta.value = event.deltaY
@@ -54,7 +71,7 @@ const handleWheel = (event) => {
       }else{
         desplazamiento.value--
       }
-      desplazamiento.value = desplazamiento.value <= 0 ? 0 : desplazamiento.value > 16374 ? 16374 : desplazamiento.value
+      desplazamiento.value = desplazamiento.value <= 0 ? 0 : desplazamiento.value > 16384 ? 16384 : desplazamiento.value
       event.preventDefault();
     }
 
@@ -65,9 +82,6 @@ watch(aBuscar, ()=>{
     desplazamiento.value = pc.value - 3 
 })
 
- watch(memoriaRam, ()=>{
-  cantIns.value = memoriaRam.value.filter(i => i !== 0).length
-}) 
 
 ///////////////////////////////////////////////////////////////////////////////
 const cargar = () => {
@@ -85,15 +99,13 @@ const cargar = () => {
   if (errores.value.length === 0) {
     
     lineas.forEach((valor, i) => {
-
-
       memoriaRam.value[ini + i] = hexadecimalADecimalConSigno(valor)
     })
     pc.value = ini
     desplazamiento.value = pc.value >= 0 && pc.value < 3 ? pc.value : pc.value - 3
-   
   }
-};
+
+}
 
 ///////////////////////////////////////////////////////////////////////////////////
 const recorrer = (esRecorrer) => {
@@ -102,100 +114,103 @@ const recorrer = (esRecorrer) => {
   /* for (let i = 0; i < contarIns; i++) {
    
   } */
-
-  let insBin = decimalABinarioC2(memoriaRam.value[pc.value]) 
-  
 //seguir con ejecutar todo,contar lineas, subir archivo cargar en textarea drag and drop, ver lenguaje assembly
   
-  const ins = {
-    opcode: insBin.substring(0, 4),
-    dr: parseInt(insBin.substring(4, 7), 2),
-    sr: parseInt(insBin.substring(4, 7), 2),
-    sr1: parseInt(insBin.substring(7, 10), 2),
-    baseR: parseInt(insBin.substring(7, 10), 2),
-    bitImm5: parseInt(insBin.substring(10, 11), 2),
-    imm5: binarioC2ADecimal(insBin.substring(11, 16)),
-    sr2: parseInt(insBin.substring(13, 16), 2),
-    PCoffset9: binarioC2ADecimal(insBin.substring(7, 16)),
-    PCoffset11: binarioC2ADecimal(insBin.substring(5, 16)),
-    n: insBin[4],
-    z: insBin[5],
-    p: insBin[6],
-    offset6: binarioC2ADecimal(insBin.substring(10, 16))
-  }
-
-  registros.value = registros.value.map((i) => hexadecimalADecimalConSigno(i))
-
-  if (pc.value > 16384) 
-    pc.value = 16384
-  else 
-    {
-      pc.value++
-      desplazamiento.value = pc.value - 3 //ver para 0000
+  if (memoriaRam.value[pc.value] === 0) {
+    errorIns.value = true
+  } else {
+    let insBin = decimalABinarioC2(memoriaRam.value[pc.value])
+    const ins = {
+      opcode: insBin.substring(0, 4),
+      dr: parseInt(insBin.substring(4, 7), 2),
+      sr: parseInt(insBin.substring(4, 7), 2),
+      sr1: parseInt(insBin.substring(7, 10), 2),
+      baseR: parseInt(insBin.substring(7, 10), 2),
+      bitImm5: parseInt(insBin.substring(10, 11), 2),
+      imm5: binarioC2ADecimal(insBin.substring(11, 16)),
+      sr2: parseInt(insBin.substring(13, 16), 2),
+      PCoffset9: binarioC2ADecimal(insBin.substring(7, 16)),
+      PCoffset11: binarioC2ADecimal(insBin.substring(5, 16)),
+      n: insBin[4],
+      z: insBin[5],
+      p: insBin[6],
+      offset6: binarioC2ADecimal(insBin.substring(10, 16))
     }
 
-  switch (ins.opcode) {
-    case "0001": //add
-      registros.value[ins.dr] = registros.value[ins.sr1] + (ins.bitImm5 === 0 ? registros.value[ins.sr2] : ins.imm5);
-      signo.value = Math.sign(registros.value[ins.dr]);
-      break;
-    case "0101": //and
-      registros.value[ins.dr] = registros.value[ins.sr1] & (ins.bitImm5 === 0 ? registros.value[ins.sr2] : ins.imm5);
-      signo.value = Math.sign(registros.value[ins.dr]);
-      break;
-    case "1001": //not
-      registros.value[ins.dr] = ~registros.value[ins.sr1];
-      signo.value = Math.sign(registros.value[ins.dr]);
-      break;
-    case "0000": //br
-      if (
-        (ins.n === "1" && signo.value === -1) ||
-        (ins.z === "1" && signo.value === 0) ||
-        (ins.p === "1" && signo.value === 1)
-      )
-        pc.value = pc.value + ins.PCoffset9
+    registros.value = registros.value.map((i) => hexadecimalADecimalConSigno(i))
+
+    if (pc.value > 16384) 
+      pc.value = 16384
+    else 
+      {
+        pc.value++
+        desplazamiento.value = pc.value - 3 //ver para 0000
+      }
+
+    switch (ins.opcode) {
+      case "0001": //add
+        registros.value[ins.dr] = registros.value[ins.sr1] + (ins.bitImm5 === 0 ? registros.value[ins.sr2] : ins.imm5);
+        signo.value = Math.sign(registros.value[ins.dr]);
+        break;
+      case "0101": //and
+        registros.value[ins.dr] = registros.value[ins.sr1] & (ins.bitImm5 === 0 ? registros.value[ins.sr2] : ins.imm5);
+        signo.value = Math.sign(registros.value[ins.dr]);
+        break;
+      case "1001": //not
+        registros.value[ins.dr] = ~registros.value[ins.sr1];
+        signo.value = Math.sign(registros.value[ins.dr]);
+        break;
+      case "0000": //br
+        if (
+          (ins.n === "1" && signo.value === -1) ||
+          (ins.z === "1" && signo.value === 0) ||
+          (ins.p === "1" && signo.value === 1)
+        )
+          pc.value = pc.value + ins.PCoffset9
+          desplazamiento.value = pc.value >= 0 && pc.value < 3 ? pc.value : pc.value - 3
+        break;
+      case "1100": //jmp ret
+        pc.value = registros.value[ins.baseR];
         desplazamiento.value = pc.value >= 0 && pc.value < 3 ? pc.value : pc.value - 3
-      break;
-    case "1100": //jmp ret
-      pc.value = registros.value[ins.baseR];
-      desplazamiento.value = pc.value >= 0 && pc.value < 3 ? pc.value : pc.value - 3
-      break;
-    case "0100": //jsr jsrr
-      let temp = pc.value;
-      pc.value =
-        parseInt(insBin.substring(4, 5), 2) === 0
-          ? registros.value[ins.baseR]
-          : pc.value + ins.PCoffset11;
-      registros.value[7] = temp;
-      desplazamiento.value = pc.value >= 0 && pc.value < 3 ? pc.value : pc.value - 3
-      break;
-    case "0010": //ld
-      registros.value[ins.dr] = memoriaRam.value[pc.value + ins.PCoffset9];
-      break;
-    case "1010": //ldi
-      registros.value[ins.dr] = memoriaRam.value[memoriaRam.value[pc.value + ins.PCoffset9]];
-      break;
-    case "0110": //ldr
-      registros.value[ins.dr] = memoriaRam.value[registros.value[ins.baseR] + ins.offset6];
-      break;
-    case "1110": //lea
-      registros.value[ins.dr] = pc.value + ins.PCoffset9;
-      break;
-    case "0011": //st
-      memoriaRam.value[pc.value + ins.PCoffset9] = registros.value[ins.sr];
-      break;
-    case "1011": //sti
-      memoriaRam.value[memoriaRam.value[pc.value + ins.PCoffset9]] = registros.value[ins.sr];
-      break;
-    case "0111": //str
-      memoriaRam.value[registros.value[ins.baseR] + ins.offset6] = registros.value[ins.sr];
-      break;
-    default:
-      errorIns.value = true
-      break;
+        break;
+      case "0100": //jsr jsrr
+        let temp = pc.value;
+        pc.value =
+          parseInt(insBin.substring(4, 5), 2) === 0
+            ? registros.value[ins.baseR]
+            : pc.value + ins.PCoffset11;
+        registros.value[7] = temp;
+        desplazamiento.value = pc.value >= 0 && pc.value < 3 ? pc.value : pc.value - 3
+        break;
+      case "0010": //ld
+        registros.value[ins.dr] = memoriaRam.value[pc.value + ins.PCoffset9];
+        break;
+      case "1010": //ldi
+        registros.value[ins.dr] = memoriaRam.value[memoriaRam.value[pc.value + ins.PCoffset9]];
+        break;
+      case "0110": //ldr
+        registros.value[ins.dr] = memoriaRam.value[registros.value[ins.baseR] + ins.offset6];
+        break;
+      case "1110": //lea
+        registros.value[ins.dr] = pc.value + ins.PCoffset9;
+        break;
+      case "0011": //st
+        memoriaRam.value[pc.value + ins.PCoffset9] = registros.value[ins.sr];
+        break;
+      case "1011": //sti
+        memoriaRam.value[memoriaRam.value[pc.value + ins.PCoffset9]] = registros.value[ins.sr];
+        break;
+      case "0111": //str
+        memoriaRam.value[registros.value[ins.baseR] + ins.offset6] = registros.value[ins.sr];
+        break;
+      default:
+        errorIns.value = true
+        break;
+    }
+
+    registros.value = registros.value.map((i) => decimalConSignoAHexadecimal(i))
   }
 
-  registros.value = registros.value.map((i) => decimalConSignoAHexadecimal(i));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -224,9 +239,9 @@ const decimalABinarioC2 = (decimal) => {
 
 function decimalConSignoAHexadecimal(sdecimal) {
   if (sdecimal >= 0) {
-    return sdecimal.toString(16).toUpperCase().padStart(16 / 4, '0');
+    return sdecimal.toString(16).toUpperCase().padStart(4, '0')
   } else {
-    return (Math.pow(2, 16) + sdecimal).toString(16).toUpperCase().padStart(16 / 4, '0');
+    return (Math.pow(2, 16) + sdecimal).toString(16).toUpperCase().padStart(4, '0')
   }
 }
 
@@ -252,13 +267,6 @@ function hexadecimalADecimalConSigno(hex) {
   }
 }
 
-const limpiarMemoria = () => {
-  errorIns.value = false
-  memoriaRam.value = new Int16Array(16384)
-}
-const limpiarRegistros = () => {
-  registros.value = new Array(8).fill("0000")
-}
 
 const reiniciarPc = computed(()=>{
   errorIns.value = false
@@ -269,30 +277,46 @@ const reiniciarPc = computed(()=>{
 //const indiceRecorrer = computed(() => decimalConSignoAHexadecimal(pc.value).padStart(4, "0") ) 
 
 
-//const contarIns = computed(() => (insTextoHexa.texto.trim() === '') ? 0 : insTextoHexa.texto.split('\n').filter(line => line.trim() !== '').length)
+const contarIns = computed(() => (insTextoHexa.texto.trim() === '') ? 0 : insTextoHexa.texto.split('\n').filter(line => line.trim() !== '').length)
 
 
 ////////////////////////////////////////////////////////////////////////////////
 
 
-const ramDecimalAHexa = computed(() => decimalConSignoAHexadecimal(memoriaRam.value[pc.value-1]))
+const ramDecimalAHexa = computed(() => decimalConSignoAHexadecimal(memoriaRam.value[pc.value]))
   
 
 ///////////////////////////////////////////////////////////////////////
     // Función para actualizar el valor en memoria
-  const updateMemory = (index, newValue) => {
+const updateMemory = (index, newValue) => {
       // Convierte el valor hexadecimal a decimal antes de guardarlo
       const decimalValue = parseInt(newValue, 16) || 0
       memoriaRam.value[index] = decimalValue
     }
 
-
-  const visibleRows = computed(() => {
-      const endIndex = Math.min(startIndex.value + rowsPerPage + buffer, totalRows);
-      return memoriaRam.value.slice(startIndex.value, endIndex);
-    });
 ////////////////////////////////////////////////////////////////////////////////////
 
+
+function handleFileDrop(event) {
+      const file = event.dataTransfer.files[0]; // Obtener el archivo
+      if (file && file.type === 'text/plain') {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          processFileContent(e.target.result); // Procesar el contenido del archivo
+        };
+        reader.readAsText(file); // Leer el archivo como texto
+      }
+    }
+function processFileContent(content) {
+      fileContent = content; // Asignar el contenido del archivo al textarea
+
+      // Si deseas recorrer cada línea del archivo
+      const lines = content.split('\n');
+      lines.forEach((line, index) => {
+        console.log(`Línea ${index + 1}: ${line}`);
+      });
+    }
+  
 
 </script>
 
@@ -314,15 +338,17 @@ const ramDecimalAHexa = computed(() => decimalConSignoAHexadecimal(memoriaRam.va
             <input v-model="inicio" maxlength="4" type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
             <label for="floatingInput">Dirección de inicio:</label>
           </div>
-          
+
           <textarea
             v-model="insTextoHexa.texto"
             class="numbered"
             style="height: 400px"
+    
           > </textarea>
+          <h5>Cantidad de instrucciones: {{contarIns}}</h5>
           <button type="submit" class="btn btn-primary">Cargar a memoria ⤴</button>
         </form>
-    
+        
         <div v-if="errores.length > 0" class="mt-2">
           <div class="alert alert-danger" role="alert">Errores de carga:</div>
           <ul v-for="item in errores" class="list-group">
@@ -335,7 +361,7 @@ const ramDecimalAHexa = computed(() => decimalConSignoAHexadecimal(memoriaRam.va
       </div>
 
       <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-4	col-xxl-4  animate__animated animate__fadeInUp">
-        <h4>Memoria RAM {{ cantIns }}/{{ memoriaRam.length }} </h4>
+        <h4>Memoria RAM {{ memoriaRam.length }} </h4>
         <div class="row justify-content-between">
           <button @click="limpiarMemoria" type="button" class="col btn btn-danger m-2">
             Limpiar memoria
@@ -352,7 +378,7 @@ const ramDecimalAHexa = computed(() => decimalConSignoAHexadecimal(memoriaRam.va
         </div>
         
         <div v-show="errorIns" class="alert alert-danger" role="alert">
-          ⛔ Instrucción no reconocida: "{{ ramDecimalAHexa }}" ⛔
+          ⛔  Ejecución detenida  ⛔
         </div>
         <table @wheel="handleWheel" class="table table-hover">
           <thead>
@@ -365,10 +391,10 @@ const ramDecimalAHexa = computed(() => decimalConSignoAHexadecimal(memoriaRam.va
           <tbody>
             <tr
               v-for="(item, index) in memoriaRam" 
-              v-show="(desplazamiento <= index) && (index<=desplazamiento+7)"
+              :class="[(desplazamiento <= index) && (index<=desplazamiento+6)? 'd-table-row' : 'd-none']"
             >
               <td 
-                @click="pc = index; errorIns = false" 
+                @click="pc = index; errorIns = false;" 
                 :class="[pc == index ? 'table-primary' : 'table-ligth']"
                 >
                 {{ decimalSinSignoAHexadecimal(index)}}
@@ -376,7 +402,7 @@ const ramDecimalAHexa = computed(() => decimalConSignoAHexadecimal(memoriaRam.va
               
               <td>
                 <input maxlength="4" :value="decimalConSignoAHexadecimal(item)" 
-                @input="updateMemory(index, $event.target.value)" type="text"
+                @input="updateMemory(index,$event.target.value)" type="text"
                 @focus="$event.target.select()"
                 style="width: 50px; text-align:center"
                 >
@@ -427,5 +453,12 @@ textarea.numbered {
   color: black;
   background-position: left calc(0.046%);
   background-size: 39px;
+}
+
+.mostrar{
+  display:block
+}
+.ocultar{
+  display: none
 }
 </style>
